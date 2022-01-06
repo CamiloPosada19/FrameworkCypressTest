39aea33d64cabfeaa1f438804575e15c72285ccb
Camilo <camilo.posada.acosta@gmail.com>
2022-01-06 17:52:21 +0100

    Dynamic Book Search Test

---
+++ cypress/integration/SearchBooksTest.js
@@ -0,0 +1,26 @@
+import { before } from "lodash";
+import SearchBoooksPages from "../support/Pages/SearchBooksPages";
+///<reference types="cypress"/>///<reference types="cypress"/>
+
+
+// Objects 
+const searchBooksPages= new SearchBoooksPages();
+   
+describe("Search inputs on the UI, it shoud verify that the search",function(){
+
+    it("Entry enviroment",function(){
+        searchBooksPages.entryEnviroment()
+    })
+    it("Make an entry and verify the results",function(){
+        searchBooksPages.makeAText("Git")
+        searchBooksPages.verifySearch(1,"Git") 
+        searchBooksPages.makeAText("de")
+        searchBooksPages.verifyDynamicsElements()
+    })
+
+    it("Check an entry that does not contain books",function(){
+        searchBooksPages.makeAText("zzzzzz")
+        searchBooksPages.verifySearchWithoutResults()
+    })
+    
+})
---
+++ cypress/support/Pages/SearchBooksPages.js
@@ -0,0 +1,49 @@
+
+///<reference types="cypress"/>///<reference types="cypress"/>
+
+const enviroment="https://demoqa.com/books"
+
+const dymamicText=[
+    "Git Pocket Guide",
+    "Learning JavaScript Design Patterns",
+    "Designing Evolvable Web APIs with ASP.NET",
+    "Understanding ECMAScript 6"
+]
+// Locators
+const inputBookId="#searchBox"
+const tableResultsBooksClass=".mr-2"
+const textElementsEmpty=".rt-noData"
+
+
+class SearchBoooksPages{
+    entryEnviroment(){
+        cy.visit(enviroment)
+    }
+    makeAText(element){
+        cy.get(inputBookId)
+        .clear()
+        .type(element)
+
+    }
+    verifySearch(expectBooks,expectText){
+        cy.get(tableResultsBooksClass)
+        .should("have.length",expectBooks)
+        .should("contain.text",expectText)
+    }
+//Verify the example in the documentation of the technical exercise. 
+    verifyDynamicsElements(){
+        cy.get(tableResultsBooksClass).each(((item,index,list)=>{
+            expect(list).to.have.length(4)
+            expect(Cypress.$(item).text()).to.eq(dymamicText[index])
+        }))
+    }
+
+    verifySearchWithoutResults(){
+        cy.get(textElementsEmpty)
+        .should("be.visible")
+        .should("contain.text","No rows found")
+    }
+
+}
+
+export default SearchBoooksPages;
